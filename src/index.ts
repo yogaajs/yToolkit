b706b67cf56c36852f178766e603fb0ab40c205e
YogaaJS <dev.yogaa@gmail.com>
2025-05-06 12:34:55 +0000

    rename package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './classes/memory-cache.class';
 export * from './classes/logger.class';
 export * from './classes/gate.class';
 export * from './classes/locker.class';
+export * from './classes/memory-cache.class';
